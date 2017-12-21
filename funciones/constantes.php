b97c367fdf0a827c559a38109d2724dc19064dc9
Andres Felipe Muñoz Marin <a.munoz@crossware.co>
2017-12-21 21:05:47 +0100

    Ultimos ajustes de imagen CMYK

--- funciones/constantes.php
+++ funciones/constantes.php
@@ -1,6 +1,7 @@
 <?php
 
     define('DIRECTORIO',"/var/www/html/dev/imagen-editor/img/editor");
+    define('DIRECTORIO_PERFILES',"/var/www/html/dev/imagen-editor/img/perfiles/");
     define('DIRECTORIO_WEB',"http://dev.crossware.co/imagen-editor/img/editor");
 
 ?>
--- funciones/guardar-imagen.php
+++ funciones/guardar-imagen.php
@@ -18,8 +18,20 @@ $url_web = str_replace($url, DIRECTORIO, DIRECTORIO_WEB."/".$nombre);
 if(is_dir(DIRECTORIO)){
 
     file_put_contents($url, $data);
+
+    $nombre_copia = $_REQUEST['nombre']."".$fecha."CMYK.jpg";
+
+    $image = new Imagick();
+    $image->readImage($url_web);
+    $image->setImageColorSpace(Imagick::COLORSPACE_CMYK);
+    $image->profileImage('icc', file_get_contents(DIRECTORIO_PERFILES."USWebCoatedSWOP.icc"));
+    $image->negateImage(FALSE, imagick::COLOR_CYAN);
+    $image->negateImage(FALSE, imagick::COLOR_MAGENTA);
+    $image->negateImage(FALSE, imagick::COLOR_YELLOW);
+    $image->negateImage(FALSE, imagick::COLOR_BLACK);
+    $image->writeImage(DIRECTORIO."/".$nombre_copia);
     
-    $pdf = new PDF(DIRECTORIO_WEB, $formato, $nombre);
+    $pdf = new PDF(DIRECTORIO_WEB, $formato, $nombre_copia);
     $pdf->generaPDF();
 
 }else{
